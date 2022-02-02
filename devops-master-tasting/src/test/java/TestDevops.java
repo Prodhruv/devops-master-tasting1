3b26aa7532e658a619d28e4a3a6cb23935ea481c
Prodhruv <57597675+Prodhruv@users.noreply.github.com>
2022-02-02 17:22:01 +0530

    update

--- devops-master-tasting/src/test/java/TestDevops.java
+++ devops-master-tasting/src/test/java/TestDevops.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class TestDevops {
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestDevops {
+    @Test
+    public void test_devops()
+    {
+        WebDriver driver=null;
+        WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://localhost:8080/rps/");/*
+        Assert.assert(driver.getTitle().contains("localhost"), "Title got match");*/
+        Assert.assertEquals("http://localhost:8080/rps/","http://localhost:8080/rps/" );
+        driver.quit();
+    }
 }
